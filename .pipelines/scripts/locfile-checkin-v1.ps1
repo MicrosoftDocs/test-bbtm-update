87e1a12e3e4475acc21b01a2f7db349bb7b7960d
CSI Users <csiusers@microsoft.com>
2023-11-09 22:25:04 -0800

    a

--- .pipelines/scripts/locfile-checkin-v1.ps1
+++ .pipelines/scripts/locfile-checkin-v1.ps1
@@ -31,9 +31,9 @@ function Main
 
     $pipelineId = $env:SYSTEM_DEFINITIONID
     $buildNumber = $env:BUILD_BUILDNUMBER
-    $commitMessage = "Loc check-in of files from pipeline $pipelineId, build $buildNumber"
-    $pullRequestTitle = "Loc check-in of files from pipeline $pipelineId, build $buildNumber"
-    $pullRequestBody = "Loc check-in of files from pipeline $pipelineId, build $buildNumber"
+    $commitMessage = "Loc check-in from pipeline $pipelineId, build $buildNumber"
+    $pullRequestTitle = "Loc check-in from pipeline $pipelineId, build $buildNumber"
+    $pullRequestBody = "Loc check-in from pipeline $pipelineId, build $buildNumber"
 
     Set-Location -Path $repoRoot
 
